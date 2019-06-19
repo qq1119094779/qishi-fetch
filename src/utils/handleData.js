41bef9d663f84ea877ee9f8265c76a60d057701a
lixin <lixin@playhudong.com>
2019-06-19 11:37:53 +0800

    修改报错返回方式

--- src/utils/handleData.js
+++ src/utils/handleData.js
@@ -1,6 +1,10 @@
 // 对请求结果进行处理：fetch请求成功后返回的是json对象
 export function parseJSON (response) {
-  return response.json()
+    try {
+      return response.json()
+    } catch (e) {
+    }
+    return response
 }
 
 // 处理get请求，传入参数对象拼接
